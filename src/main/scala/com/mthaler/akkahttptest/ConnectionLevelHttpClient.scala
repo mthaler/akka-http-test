7a3f8c8efd3c5cc86e7dce75990105794e97d785
Michael Thaler <michael.thaler@dlr.de>
2016-02-19 10:46:35 +0100

    Unmarshall response.

--- src/main/scala/com/mthaler/akkahttptest/ConnectionLevelHttpClient.scala
+++ src/main/scala/com/mthaler/akkahttptest/ConnectionLevelHttpClient.scala
@@ -3,8 +3,10 @@ package com.mthaler.akkahttptest
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.{BasicHttpCredentials, Authorization}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
+import akka.http.scaladsl.unmarshalling.Unmarshal
 import scala.concurrent.Future
 
 object ConnectionLevelHttpClient extends App {
@@ -13,16 +15,21 @@ object ConnectionLevelHttpClient extends App {
   implicit val materializer = ActorMaterializer()
   implicit val ec = system.dispatcher
 
+  val auth = Authorization(BasicHttpCredentials("test", "test"))
+
   val connectionFlow: Flow[HttpRequest, HttpResponse, Future[Http.OutgoingConnection]] =
     Http().outgoingConnection("127.0.0.1", 8888)
-  val responseFuture =
-    Source.single(HttpRequest(uri = "/test.txt"))
+  val responseFuture: Future[String] =
+    Source.single(HttpRequest(uri = "/test.txt", headers = List(auth)))
       .via(connectionFlow)
-      .runWith(Sink.head).map { response =>
+      .runWith(Sink.head).flatMap { response =>
       if (response.status == StatusCodes.OK) {
-        println("Result: " + response.entity)
+        Unmarshal(response).to[String]
       } else {
-        println("Error: " + response.status)
+        Future.successful("Error: " + response.status)
       }
     }
+  responseFuture.onSuccess {
+    case result => println(result)
+  }
 }
