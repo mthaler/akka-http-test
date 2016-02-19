3dd44d08421ebf9da845b3b48941dec9d5190adb
Michael Thaler <michael.thaler@dlr.de>
2016-02-19 09:42:44 +0100

    Implemented test HTTP server.

---
+++ src/main/scala/com/mthaler/akkahttptest/HttpServer.scala
@@ -0,0 +1,45 @@
+package com.mthaler.akkahttptest
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.directives.Credentials
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+
+object HttpServer extends App {
+
+  implicit val system = ActorSystem("system")
+  implicit val mat = ActorMaterializer()
+
+  // credentials used to authenticate users
+  val credentials = Map("test" -> ("test", Set("user")))
+
+  // authenticator used to authenticate users, uses credentials
+  def authenticator[T](cred: Credentials) : Option[Set[String]] =
+    cred match {
+      case cred@Credentials.Provided(name) =>
+        credentials.get(name).flatMap {
+          case (pass, roles) =>
+            if(cred.verify(pass))
+              Some(roles)
+            else
+              None
+        }
+      case Credentials.Missing =>
+        None
+    }
+
+  val route = authenticateBasic("HTTP test server", authenticator) { roles ⇒
+    get {
+      path ("test.txt") {
+        complete ("Hello, World")
+      }
+    }
+  }
+
+  val binding = Http(system).bindAndHandle(
+    interface = "0.0.0.0",
+    port = 8888,
+    handler = route
+  )
+}
