35a2e47265d92a56ec5d7120e037e33623a924e9
Michael Thaler <michael.thaler@dlr.de>
2016-02-19 15:33:23 +0100

    Added ConnectionLevelHttpsClient.scala.

---
+++ src/main/scala/com/mthaler/akkahttptest/ConnectionLevelHttpsClient.scala
@@ -0,0 +1,57 @@
+package com.mthaler.akkahttptest
+
+import java.security.cert.X509Certificate
+import javax.net.ssl.{KeyManager, X509TrustManager, SSLContext}
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.{HttpsContext, Http}
+import akka.http.scaladsl.model.{StatusCodes, HttpResponse, HttpRequest}
+import akka.http.scaladsl.model.headers.{BasicHttpCredentials, Authorization}
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source, Flow}
+
+import scala.concurrent.Future
+
+object ConnectionLevelHttpsClient extends App {
+  implicit val system = ActorSystem()
+  implicit val materializer = ActorMaterializer()
+  implicit val ec = system.dispatcher
+
+  val auth = Authorization(BasicHttpCredentials("test", "test"))
+
+  private val trustfulSslContext: SSLContext = {
+
+    object NoCheckX509TrustManager extends X509TrustManager {
+      override def checkClientTrusted(chain: Array[X509Certificate], authType: String) = ()
+      override def checkServerTrusted(chain: Array[X509Certificate], authType: String) = ()
+      override def getAcceptedIssuers = Array[X509Certificate]()
+    }
+
+    val context = SSLContext.getInstance("TLS")
+    context.init(Array[KeyManager](), Array(NoCheckX509TrustManager), null)
+    context
+  }
+
+  val trustfulClientContext: HttpsContext =
+    HttpsContext(trustfulSslContext)
+
+  val connectionFlow: Flow[HttpRequest, HttpResponse, Future[Http.OutgoingConnection]] =
+    Http().outgoingConnectionTls("localhost", 6443, httpsContext = Some(trustfulClientContext))
+  val responseFuture: Future[String] =
+    Source.single(HttpRequest(uri = "/test.txt", headers = List(auth)))
+      .via(connectionFlow)
+      .runWith(Sink.head).flatMap { response =>
+      if (response.status == StatusCodes.OK) {
+        Unmarshal(response).to[String]
+      } else {
+        Future.successful("Error: " + response.status)
+      }
+    }
+  responseFuture.onSuccess {
+    case result => println(result)
+  }
+  responseFuture.onFailure {
+    case ex => println("Exception: " + ex)
+  }
+}
